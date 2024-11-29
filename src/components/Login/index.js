e790b39874f65229e14bbb0dc847b92d4b8b3e03
P Sai Karthik <141780456+SaiKarthik1235@users.noreply.github.com>
2024-11-29 21:38:48 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -38,9 +38,9 @@ class Login extends Component {
     event.preventDefault()
     let { usernameInput, passwordInput } = this.state
 
-    if (usernameInput.toLowerCase().trim('') === 'tarun')
+    if (usernameInput.toLowerCase().trim('') === 'karthik')
       usernameInput = 'rahul'
-    if (passwordInput === 'tarun@9849') passwordInput = 'rahul@2021'
+    if (passwordInput === 'karthik@1234') passwordInput = 'rahul@2021'
 
     const userDetails = { username: usernameInput, password: passwordInput }
     const LoginApiUrl = 'https://apis.ccbp.in/login'
@@ -74,7 +74,7 @@ class Login extends Component {
         <LoginInputField
           type="text"
           value={usernameInput}
-          placeholder="tarun"
+          placeholder="karthik"
           id="username"
           onChange={this.updateUsername}
         />
@@ -90,7 +90,7 @@ class Login extends Component {
         <LoginInputField
           type={showPassword ? 'text' : 'password'}
           value={passwordInput}
-          placeholder="tarun@9849"
+          placeholder="karthik@1234"
           id="password"
           onChange={this.updatePassword}
         />
